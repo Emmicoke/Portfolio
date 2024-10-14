7c8cb950bd8a29f0ff1a097896c8d611d02f0a0e
Emmicoke <fahrettineminkorkut@gmail.com>
2024-10-14 20:46:56 +0300

    modified:   src/data/aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,5 @@
 export const aboutData = {
-    title: "Who am I",
+    title: "Who am I?",
     description1: "Hello! My name is Fahrettin Emin Korkut. I'm a student at Hasan Kalyoncu University in Turkey.",
     description2: "I am Software Engineering student at Hasan Kalyoncu University. I'm interested in Web Development and Design, Mobile app development, Database management and Image processing",
     image: 2

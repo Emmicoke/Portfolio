27aa8f291ebf1634640b758ae18829730ca418ea
Emmicoke <fahrettineminkorkut@gmail.com>
2025-08-08 15:25:55 +0300

    Updated the about me data.

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who am I?",
     description1: "Hello! My name is Fahrettin Emin Korkut. I'm a student at Hasan Kalyoncu University in Turkey.",
-    description2: "I am Software Engineering student at Hasan Kalyoncu University. I'm interested in Web Development and Design, Mobile app development, Database management and Image processing",
+    description2: "I’m Fahrettin Emin Korkut, a passionate Software Engineering student at Hasan Kalyoncu University with a strong interest in web development, mobile applications, and image processing. Over the years, I’ve gained experience in C++, C, Python, SQL, React, React Native, HTML, CSS, and JavaScript, working on projects ranging from academic assignments to real-world applications. I’ve participated in international programs such as Erasmus+, completing an internship in Spain and education in Czech Republic, and I’ve worked on various projects, including booking systems, review management platforms, and educational exam applications. I enjoy solving problems, learning new technologies, and turning ideas into functional, user-friendly solutions.",
     image: 2
 }
